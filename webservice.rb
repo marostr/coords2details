a058403f9e44be016e50c60e2a6ca715871b3716
Marcin Ostrowski <marcin.ostrowski@netguru.pl>
2014-11-16 13:11:48 +0100

    Updated get info

--- webservice.rb
+++ webservice.rb
@@ -3,7 +3,7 @@ require_relative 'details_checker'
 
 class WebService < Sinatra::Base
   get '/' do
-    'Please use GET /coords2details and provide locations to check in following form: ?locations=30.598578,31.059259:30.195844,31.001581:30.921524,31.261027'
+    "Please use GET /coords2details and provide locations to check in following form: ?locations=30.598578,31.059259:30.195844,31.001581:30.921524,31.261027\nOr POST /coords2details and provide locations in json. Example [[30.594324, 31.052343], [31.0012532,30.92132]]"
   end
 
   get '/coords2details' do
